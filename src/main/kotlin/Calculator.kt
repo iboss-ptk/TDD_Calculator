fc6229bf1fee84e142d8e4dac8b97e31ab6ed5ea
anuchitop <anuchitp@thoughtworks.com>
2020-04-16 15:53:09 +0700

    add 1 + 0 case

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,3 +1,6 @@
 fun calculate(expression: String): String {
+    if ("1 + 0" == expression) {
+        return "1"
+    }
     return "2"
 }
--- src/test/kotlin/CalculatorTests.kt
+++ src/test/kotlin/CalculatorTests.kt
@@ -2,5 +2,6 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class CalculatorTests : FunSpec({
-    test("1 + 1 equals 2") { calculate("1 + 1") shouldBe 2 }
+    test("1 + 1 equals 2") { calculate("1 + 1") shouldBe "2" }
+    test("1 + 0 equals 1") { calculate("1 + 0") shouldBe "1" }
 })
